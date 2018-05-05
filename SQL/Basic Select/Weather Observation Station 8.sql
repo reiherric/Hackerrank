ef74ec63be2fd4b464ccc5930b28f2e3f8bfea2c
Reinaldo Hernandez Ricardo <reiherric@outlook.com>
2018-05-05 11:19:44 -0400

    Create Weather Observation Station 8.sql

---
+++ SQL/Basic Select/Weather Observation Station 8.sql
@@ -0,0 +1,28 @@
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+Input Format
+The STATION table is described as follows:
+
+STATION
+FIELD     FORMAT
+ID        NUMBER
+CITY      VARCHAR2(21)
+STATE     VARCHAR2(2)
+LAT_N     NUMBER
+lONG_W    NUMBER
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+/*MS SQL Server*/
+
+SELECT CITY
+FROM STATION
+INNER JOIN 
+(SELECT CITY as VOWEL_ENDING
+FROM STATION
+WHERE CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U'
+) ENDING_QUERY
+ON CITY = ENDING_QUERY.VOWEL_ENDING
+WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%'
+GROUP BY CITY
